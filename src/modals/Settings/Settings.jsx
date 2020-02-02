8b66d604501199514806bb1ed4266ddbd813f1ba
Andy <andy.speirs2@gmail.com>
2020-02-02 23:24:07 +0000

    Added clear sessionStorage button to settings

--- src/modals/Settings/Settings.jsx
+++ src/modals/Settings/Settings.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+	IconButton,
 	List,
 	ListItem,
 	ListItemSecondaryAction,
 	ListItemText,
 } from '@material-ui/core';
+import {
+	Delete as DeleteIcon,
+} from '@material-ui/icons';
 
 import Modal from '../../components/Modal';
 
@@ -23,6 +27,18 @@ const Settings = ({
 				<ListItemText primary="App version:" />
 				<ListItemSecondaryAction>
 					{`v${process.env.REACT_APP_VERSION}`}
+				</ListItemSecondaryAction>
+			</ListItem>
+			<ListItem>
+				<ListItemText primary="Clear Dates:" />
+				<ListItemSecondaryAction>
+					<IconButton
+						aria-label="delete"
+						color="secondary"
+						onClick={() => localStorage.clear()}
+					>
+						<DeleteIcon />
+					</IconButton>
 				</ListItemSecondaryAction>
 			</ListItem>
 		</List>
